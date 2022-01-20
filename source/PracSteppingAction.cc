6c5940f912ea1401a8b56cb058e6768bd03a4d37
sorimjangddol <j991005@korea.ac.kr>
2022-01-20 22:17:49 +0900

    2201202217

--- source/PracSteppingAction.cc
+++ source/PracSteppingAction.cc
@@ -10,8 +10,7 @@
 
 PracSteppingAction::PracSteppingAction(PracEventAction* eventAction)
 {
-    // 밑에서 언급중인 fScoring 관련이 필요하면 아마 여기에 fScoringVolume의 초기화가 들어가야 할텐데
-    // 일단 빼보고 해봅시다.
+    // pass
 }
 
 
@@ -24,6 +23,4 @@ PracSteppingAction::~PracSteppingAction()
 void PracSteppingAction::UserSteppingAction(const G4Step*)
 {
     G4cout << "Step is stepping." << G4endl;
-    // 아마 fScoringVolume을 Volume이랑 비교해서 밖으로 나가면 step을 종료시키는 코드가 여기 들어가는 것 같은데
-    // 일단 빼보고 해봅시다.
 }
