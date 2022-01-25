00478f63dc379f7c0870285076952ed49c9ea8a4
sorimjangddol <j991005@korea.ac.kr>
2022-01-25 14:01:01 +0900

    2201251401

--- source/PracRunAction.cc
+++ source/PracRunAction.cc
@@ -22,7 +22,7 @@ PracRunAction::~PracRunAction() {}
 void PracRunAction::BeginOfRunAction(const G4Run* run)
 {
     G4cout << "==================== Start of Run Information before start Run (Manual) ====================" << G4endl;
-    G4cout << "Number of Event : " << run->GetNumberOfEvent() << G4endl;
+    G4cout << "Number of Event                 : " << run->GetNumberOfEvent() << G4endl;
     G4cout << "Number of Event to be Processed : " << run->GetNumberOfEventToBeProcessed() << G4endl;
     G4cout << "====================  End of Run Information before start Run (Manual)  ====================" << G4endl;
 }
@@ -34,11 +34,11 @@ void PracRunAction::EndOfRunAction(const G4Run* run)
     if (nofEvents == 0) return;
     
     G4cout << "==================== Start of Run Information (Manual) ====================" << G4endl;
-    G4cout << "Number of Event : " << nofEvents << G4endl;
+    G4cout << "Number of Event                 : " << nofEvents << G4endl;
     G4cout << "Number of Event to be Processed : " << run->GetNumberOfEventToBeProcessed() << G4endl;
-    G4cout << "Run ID : " << run->GetRunID() << G4endl;
-    G4cout << "HCtable : "  << run->GetHCtable() << G4endl;
-    G4cout << "DCtable : " << run->GetDCtable() << G4endl;
-    G4cout << "Random Number Status : " << run->GetRandomNumberStatus() << G4endl;
+    G4cout << "Run ID                          : " << run->GetRunID() << G4endl;
+    G4cout << "HCtable                         : "  << run->GetHCtable() << G4endl;
+    G4cout << "DCtable                         : " << run->GetDCtable() << G4endl;
+    G4cout << "Random Number Status            : " << run->GetRandomNumberStatus() << G4endl;
     G4cout << "====================  End of Run Information (Manual)  ====================" << G4endl;
 }
