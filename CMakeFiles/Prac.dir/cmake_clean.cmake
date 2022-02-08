e1176557759307bede1717dc0d9340d35dd53283
sorimjangddol <j991005@korea.ac.kr>
2022-02-08 19:43:34 +0900

    2202081943

--- CMakeFiles/Prac.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Prac"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Prac.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
